96fdaf4374216ae2c13d32cb3e0f9eeee8386676
John Stahre <john.kenneth.kauppi@gmail.com>
2021-01-14 08:02:52 +0100

    försöker med karusellen

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 new WOW().init();
 
+// https://kenwheeler.github.io/slick/
+//https://github.com/kenwheeler/slick
 
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+$(document).ready(() => {
+  $('#slideshow .slick').slick();
+ })
+
+  $('.center').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
